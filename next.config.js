b0d218ffcab1f7ed96dc2269b9d2f8f7b83a348a
Carol Alves <carolalves@Carols-Mac-mini.local>
2026-08-26 20:49:24 -0300

    Configurando o arquivo next.config

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* suas configurações aqui */
+};
+
+export default nextConfig; // ou module.exports = nextConfig; se usar .js comum
